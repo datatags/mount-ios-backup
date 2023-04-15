87093a56de40ff6d8b8c25c5ac749297036a6bfd
datatags <datatags@users.noreply.github.com>
2023-04-15 10:22:13 -0700

    Add foreground flag

--- mount_ios_backup.py
+++ mount_ios_backup.py
@@ -9,16 +9,21 @@ from fuse import FUSE
 from standard_backup import BackupFS
 from encrypted_backup import EncryptedBackupFS
 
-def main(mountpoint, root, password=None):
+def main(mountpoint, root, password=None, foreground=False):
     plist = biplist.readPlist(os.path.join(root, "Manifest.plist"))
-    if not plist["IsEncrypted"]:
+    if plist["IsEncrypted"]:
+        print("This is an encrypted backup.")
+        if password is None:
+            password = getpass.getpass(prompt="Enter the backup password: ")
+        fs = EncryptedBackupFS(root, password)
+    else:
         print("This is an unencrypted backup.")
-        FUSE(BackupFS(root), mountpoint, nothreads=True, foreground=True)
-        return
-    print("This is an encrypted backup.")
-    if password is None:
-        password = getpass.getpass(prompt="Enter the backup password: ")
-    FUSE(EncryptedBackupFS(root, password), mountpoint, nothreads=True, foreground=True)
+        fs = BackupFS(root)
+    if foreground:
+        print("Staying in foreground, press Ctrl-C to unmount")
+    else:
+        print(f"Switching to background, use 'fusermount -u {os.path.abspath(root)}' to unmount")
+    FUSE(fs, mountpoint, nothreads=True, foreground=foreground)
 
 class PrintUsageParser(argparse.ArgumentParser):
     def error(self, message):
@@ -35,13 +40,14 @@ if __name__ == '__main__':
     supplied using the --password flag, or the BACKUP_PASSWORD
     environment variable.
     """)
-    arg_parser.add_argument("backup")
-    arg_parser.add_argument("mountpoint")
+    arg_parser.add_argument("backup", help="the backup folder to read (include device UID)")
+    arg_parser.add_argument("mountpoint", help="the folder to mount the backup in")
     arg_parser.add_argument("-p", "--password", help="the backup password, if encrypted")
+    arg_parser.add_argument("-f", "--foreground", action="store_true", help="keep the process in the foreground")
     args = arg_parser.parse_args()
     root = args.backup
     mountpoint = args.mountpoint
     password = args.password
     if password is None and "BACKUP_PASSWORD" in os.environ:
         password = os.environ["BACKUP_PASSWORD"]
-    main(mountpoint, root, password)
+    main(mountpoint, root, password, args.foreground)
